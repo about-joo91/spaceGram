ebd288631c8df3b30dad45e1fdc858309b983351
about-joo91 <about.joo91@gmail.com>
2022-05-08 11:30:23 +0900

    feature/드래그 앤 드랍으로 파일을 업로드하는 기능!

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from crypt import methods
-from flask import Flask, render_template
+
+from flask import Flask, jsonify, render_template, request
+import datetime
+from pymongo import MongoClient
+import base64
+client = MongoClient('mongodb+srv://@cluster0.qwbpf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+db = client.dbsparta
 
 app = Flask(__name__)
 
@@ -13,6 +19,19 @@ def home():
 def my_page():
     return render_template('mypage.html')
 
-
+@app.route('/posts', methods=['POST'])
+def post_file():
+    user_id = ''
+    files = request.files['img']
+    content = request.form['content']
+    file_base64 = base64.b64encode(files.read())
+    doc = {
+        'user_id': user_id,
+        'file' : file_base64,
+        'content': content,
+        'timestamp': datetime.datetime.utcnow()
+    }
+    db.posts.insert_one(doc)
+    return jsonify({'msg': '저장완료!'})
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
