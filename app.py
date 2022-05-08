74a85b1960856bc60482bfa4b5b72056c5582e24
about-joo91 <about.joo91@gmail.com>
2022-05-08 19:23:55 +0900

    edit/gitignore 업데이트 및 decorate function 추가

--- app.py
+++ app.py
@@ -1,39 +1,57 @@
-
-from flask import Flask, jsonify, render_template, request
+from functools import wraps
+from flask import Flask, jsonify, render_template, request, abort
 import datetime
 from pymongo import MongoClient
 import base64
-from PIL import Image
+import jwt
 import hashlib
 client = MongoClient('mongodb+srv://test:sparta@cluster0.qwbpf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 
+SECRET_KEY = 'spaceGram'
 db = client.dbsparta
 
 app = Flask(__name__)
-ab = hashlib.sha256('1234'.encode('utf-8')).hexdigest()
-print(ab)
+
+def authrize(f):
+    @wraps(f)
+    def decorated_function(*args, **kws):
+        if 'myToken' not in request.cookies:
+            abort(401)
+        user = None
+        token = request.cookies['myToken']
+        try:
+            user = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        except:
+            abort(401)
+        return f(user, *args, **kws)
+    return decorated_function
+    
 @app.route('/')
 def home():
     return render_template('main.html')
 
 
 @app.route('/my_page')
-def my_page():
-    return render_template('mypage.html')
+@authrize
+def my_page(user):
+    if user is not None:
+        return render_template('mypage.html')
 
 @app.route('/posts', methods=['POST'])
-def post_file():
-    user_id = ''
-    files = request.files['img']
-    content = request.form['content']
-    image_base64 = base64.b64encode(files.read())
-    doc = {
-        'user_id': user_id,
-        'file' : image_base64,
-        'content': content,
-        'timestamp': datetime.datetime.utcnow()
-    }
-    db.posts.insert_one(doc)
-    return jsonify({'msg': '저장완료!'})
+@authrize
+def post_file(user):
+    if user is not None:
+        user_id = user.get('id')
+        files = request.files['img']
+        content = request.form['content']
+        image_base64 = base64.b64encode(files.read())
+        doc = {
+            'user_id': user_id,
+            'file' : image_base64,
+            'content': content,
+            'timestamp': datetime.datetime.utcnow()
+        }
+        db.posts.insert_one(doc)
+        return jsonify({'msg': '저장완료!'})
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
