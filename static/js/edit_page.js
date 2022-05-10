9e395be468e4f05f22aec9c9c85e1e840288451f
khw7876 <skanahrwi@naver.com>
2022-05-11 06:46:18 +0900

    feature/ 홈 링크 구현

--- static/js/edit_page.js
+++ static/js/edit_page.js
@@ -28,6 +28,8 @@ document.querySelector('.summit').addEventListener('click', function(){
 })
 
 function update_user(){
+
+
     let new_user_name = $('#new_user_name').val()
     let new_nick_name = $('#new_nick_name').val()
     let new_email = $('#new_email').val()
@@ -47,4 +49,44 @@ function update_user(){
     });
 
 
+}document.querySelector('.mh_circle_avatar').addEventListener('click', function(){
+    open_profile_modal()
+})
+function open_profile_modal(){
+    document.querySelector('.profile_modal').style.display = "block";
+    document.querySelector('.mp_profile_modal').style.display = "block";
 }
+if (document.querySelector('.profile_modal').style.display == "block"){
+    document.querySelector('.mp_profile_modal').addEventListener('click', function(){
+        close_profile_modal()
+    })
+    
+}
+function close_profile_modal(){ 
+    document.querySelector('.profile_modal').style.display = "none";
+    document.querySelector('.mp_profile_modal').style.display = "none";
+
+}
+document.querySelector('.pm_my_page').addEventListener('click', function(){
+    open_my_page()})
+function open_my_page(){
+    window.location.replace('/my_page')
+}
+document.querySelector('.pm_book_mark').addEventListener('click', function(){
+    open_booK_mark()
+    close_profile_modal()})
+    // 만약 여기서 오류가 생긴다면, my_page를 먼저 불러오고 함수를 실행해야 함
+function open_booK_mark(){
+    book_mark_list()
+
+}
+function go_home(){
+    window.location.replace('/home')
+}
+document.querySelector('.mh_name').addEventListener('click', function(){
+    go_home()
+})
+document.querySelector('.bi bi-send mh_i_send').addEventListener('click', function(){
+    go_home()
+})
+
